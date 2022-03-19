a3c12fc344ba19140f3488de32953ed7df40da83
jin09 <jin09@ardbeg.c.u-tokyo.ac.jp>
2022-03-20 03:20:38 +0900

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
-
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop testing app</h2>
+import {Component} from "react";
+import Customer from "./components/Customer";
 
 
-    </div>
-  );
+const customers = [
+    {
+        'id': 1,
+        'image':'http://placeimg.com/64/64/1',
+        'name' : '이재원',
+        'birthday' : '921222',
+        'gender' : 'Male',
+        'job' : 'student'
+
+    },
+    {
+        'id': 2,
+        'image':'http://placeimg.com/64/64/2',
+        'name' : '유우키',
+        'birthday' : '920417',
+        'gender' : 'Female',
+        'job' : 'student'
+
+    },
+    {'id': 3,
+        'image':'http://placeimg.com/64/64/3',
+        'name' : 'dk',
+        'birthday' : '920317',
+        'gender' : 'Female',
+        'job' : 'student'
+
+    },
+]
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                { customers.map(c => {
+                    return (
+                        <Customer
+                            key={c.id}
+                            id={c.id}
+                            image={c.image}
+                            name={c.name}
+                            birthday={c.birthday}
+                            gender={c.gender}
+                            job={c.job}
+                        />
+                    );
+                })
+
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
